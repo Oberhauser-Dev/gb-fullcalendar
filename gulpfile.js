c55f488f2cc227eb0c8fb85f9564ca285ea1946c
Gustl22 <git@reb0.org>
2026-05-03 16:16:05 +0200

    update script

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,41 @@
-const gulp = require( 'gulp' );
-const grename = require( 'gulp-rename' );
-const gzip = require( 'gulp-zip' );
-const packageJson = require( './package.json' );
+import gulp from 'gulp';
+import gulpRename from 'gulp-rename';
+import gulpZip from 'gulp-zip';
+import * as packageJson from './package.json' with {type: 'json'};
 
-async function zip( snapshot = false ) {
-	let suffix;
-	if (snapshot) {
-		suffix = await new Promise( ( resolve, reject ) => {
-			const { exec } = require( 'child_process' );
-			exec( 'git rev-parse --abbrev-ref HEAD', ( err, stdout, stderr ) => {
-				if (err) {
-					reject( err );
-				}
-				resolve( stdout.trim() );
-			} );
-		} );
-		suffix = suffix.replace( /[\\~#%&*{}/:<>?|\"]/g, '' ) + '-SNAPSHOT';
-	} else {
-		suffix = 'v' + packageJson.version;
-	}
-	return gulp.src( [
-		'build/*',
-		'php/*',
-		'res/*',
-		'gb-fullcalendar.php',
-		'package.json',
-		'README.md',
-		'CHANGELOG.md',
-		'LICENSE.md',
-	], { base: './' } )
-		.pipe( grename( function( file ) {
-			file.dirname = 'gb-fullcalendar/' + file.dirname;
-		} ) )
-		.pipe( gzip( `gb-fullcalendar-${ suffix }.zip` ) )
-		.pipe( gulp.dest( 'dist' ) );
+export async function zip(snapshot = false) {
+    let suffix;
+    if (snapshot) {
+        suffix = await new Promise((resolve, reject) => {
+            const {exec} = require('child_process');
+            exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(stdout.trim());
+            });
+        });
+        suffix = suffix.replace(/[\\~#%&*{}\/:<>?|"]/g, '') + '-SNAPSHOT';
+    } else {
+        suffix = 'v' + packageJson.version;
+    }
+    return gulp.src([
+        'build/*',
+        'php/*',
+        'res/*',
+        'gb-fullcalendar.php',
+        'package.json',
+        'README.md',
+        'CHANGELOG.md',
+        'LICENSE.md',
+    ], {base: './'})
+        .pipe(gulpRename(function (file) {
+            file.dirname = 'gb-fullcalendar/' + file.dirname;
+        }))
+        .pipe(gulpZip(`gb-fullcalendar-${suffix}.zip`))
+        .pipe(gulp.dest('dist'));
 }
 
-exports.zip = () => zip( false );
-exports.zipSnapshot = () => zip( true );
+export async function zipSnapshot() {
+    await zip(true);
+}
